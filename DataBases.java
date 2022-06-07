e9cadfcdec8106c59e33b494cb280876a1fffd5d
Yeneul <105287902+Yeneul@users.noreply.github.com>
2022-06-07 20:12:19 +0900

    Update DataBases.java

--- DataBases.java
+++ DataBases.java
@@ -1,16 +1,16 @@
-package com.example.dbtest2;
+package com.example.dbtest3;
 
 import android.provider.BaseColumns;
 
 public final class DataBases {
 
     public static final class CreateDB implements BaseColumns{
-        public static final String USERID = "userid";
-        public static final String NAME = "name";
+        public static final String DEPART = "depart";
+        public static final String ARRIVAL = "arrival";
         public static final String _TABLENAME0 = "usertable";
         public static final String _CREATE0 = "create table if not exists "+_TABLENAME0+"("
                 +_ID+" integer primary key autoincrement, "
-                +USERID+" text not null , "
-                +NAME+" text not null );";
+                +DEPART+" text not null , "
+                +ARRIVAL+" text not null );";
     }
 }
